98efa137d2c4e5d62ed64697ef9aeef539c4b4e4
Gulshan <kumarjhagulshan@gmail.com>
2024-03-27 01:37:37 +0530

    Update application.py

--- application.py
+++ application.py
@@ -6,6 +6,14 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
+import logging
+from logging.handlers import RotatingFileHandler
+
+logger = logging.getLogger(__name__)
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+logger.setLevel(logging.DEBUG)
+
+
 
 application = Flask(__name__)
 app = application
@@ -21,10 +29,13 @@ def home():
 
 @app.route('/predict_datapoint',methods = ['GET','POST'])
 def predict_datapoint():
+    print(request.method)
     if request.method=='GET':
         return render_template('home.html')
     else:
-        data = CustomData(
+        try:
+            logger.debug('Starting prediction...')
+            data = CustomData(
             gender=request.form.get('gender'),
             race_ethnicity=request.form.get('ethnicity'),
             parental_level_of_education=request.form.get('parental_level_of_education'),
@@ -34,15 +45,20 @@ def predict_datapoint():
             writing_score=float(request.form.get('reading_score'))
             )
         
-        df = data.get_data_as_dataframe()
-        #print(df)
-        #print("Before Prediction")
+            df = data.get_data_as_dataframe()
+            #print(df)
+            #print("Before Prediction")
 
-        pred_pipeline = PredictPipeline()
-        results = pred_pipeline.predict(df)
-        #print("after Prediction")
-        return render_template('home.html',results=round(results[0],2))
+            pred_pipeline = PredictPipeline()
+            results = pred_pipeline.predict(df)
+            #print("after Prediction")
+            return render_template('home.html',results=round(results[0],2))
+        except Exception as e:
+            logger.debug(e)
+            
+    
+
     
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0")    
+    app.run(host="0.0.0.0", debug=True)
