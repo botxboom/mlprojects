226a5e1ba88c6d2f9d640d392e9fb9310472c31b
Gulshan <kumarjhagulshan@gmail.com>
2024-03-27 02:22:54 +0530

    Update application.py

--- application.py
+++ application.py
@@ -45,29 +45,29 @@ def home():
 @application.route('/predict_datapoint',methods = ['GET','POST'])
 def predict_datapoint():
     print("GULSHAN")
-        try:
-            print("Starting Prediction...")
-            data = CustomData(
-            gender=request.form.get('gender'),
-            race_ethnicity=request.form.get('ethnicity'),
-            parental_level_of_education=request.form.get('parental_level_of_education'),
-            lunch=request.form.get('lunch'),
-            test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('writing_score')),
-            writing_score=float(request.form.get('reading_score'))
-            )
-        
-            df = data.get_data_as_dataframe()
-            #print(df)
-            #print("Before Prediction")
+    try:
+        print("Starting Prediction...")
+        data = CustomData(
+        gender=request.form.get('gender'),
+        race_ethnicity=request.form.get('ethnicity'),
+        parental_level_of_education=request.form.get('parental_level_of_education'),
+        lunch=request.form.get('lunch'),
+        test_preparation_course=request.form.get('test_preparation_course'),
+        reading_score=float(request.form.get('writing_score')),
+        writing_score=float(request.form.get('reading_score'))
+        )
+    
+        df = data.get_data_as_dataframe()
+        #print(df)
+        #print("Before Prediction")
 
-            pred_pipeline = PredictPipeline()
-            results = pred_pipeline.predict(df)
-            #print("after Prediction")
-            return render_template('home.html',results=round(results[0],2))
-        except Exception as e:
-            print("exception")
-            print(e)
+        pred_pipeline = PredictPipeline()
+        results = pred_pipeline.predict(df)
+        #print("after Prediction")
+        return render_template('home.html',results=round(results[0],2))
+    except Exception as e:
+        print("exception")
+        print(e)
             
     
 
